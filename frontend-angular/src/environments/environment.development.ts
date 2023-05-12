654b93712d789c96137f60138ebc63877c6bed89
Frederic Esser <frederic.esser@googlemail.com>
2023-05-12 10:56:53 +0200

    added footer
    added GitHub Logo to the Project Page
    added environment to load the Project version from the package.json

---
+++ frontend-angular/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  appVersion: "v" + require('../../package.json').version + "-dev"
+};
---
+++ frontend-angular/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  appVersion: "v" + require('../../package.json').version
+};
